a12ce8e47b3a21bbb42f0d7a8f8b5c7ac323e039
sapna rathore <sapna.devops8878@gmail.com>
2024-08-06 18:16:32 -0700

    created a variable declaration file in which we used environment variable and user defined variables

---
+++ src_scripts/variable-declaration.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#we are writing a variable script today
+#accessing an environment variable
+
+echo $USER 
+
+#CREATING AND ACCESSING USER DEFINED VARIABLE
+
+user_name="sapna rathore"
+
+echo $user_name
